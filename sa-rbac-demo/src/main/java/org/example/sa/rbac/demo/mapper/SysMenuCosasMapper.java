8e6745aa8431b98313af9b140721df4adf82f1a3
郑炳初 <1023739895@qq.com>
2024-02-02 17:10:55 +0800

    权限

---
+++ sa-rbac-demo/src/main/java/org/example/sa/rbac/demo/mapper/SysMenuCosasMapper.java
@@ -0,0 +1,18 @@
+package org.example.sa.rbac.demo.mapper;
+
+import org.example.sa.rbac.demo.entity.SysMenuCosas;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 系统菜单 Mapper 接口
+ * </p>
+ *
+ * @author bingchu
+ * @since 2024-01-18
+ */
+@Mapper
+public interface SysMenuCosasMapper extends BaseMapper<SysMenuCosas> {
+
+}
